0fa34aea5e2e16496c2286da83e172aecb4d4717
mrrobot <prostomax530@gmail.com>
2020-10-18 23:13:31 +0500

    add migrations

--- migrations/Version20201018181249.php
+++ migrations/Version20201018181249.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201017171142 extends AbstractMigration
+final class Version20201018181249 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,10 +20,7 @@ final class Version20201017171142 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SEQUENCE bookmark_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE bookmark (id INT NOT NULL, date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, favicon BYTEA DEFAULT NULL, url VARCHAR(255) NOT NULL, page_title TEXT DEFAULT NULL, meta_description TEXT DEFAULT NULL, meta_keywords TEXT DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
@@ -32,7 +29,5 @@ final class Version20201017171142 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE bookmark_id_seq CASCADE');
-        $this->addSql('DROP TABLE bookmark');
     }
 }
